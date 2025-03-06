58291e621072a7aa3677c3c25e2d614216001feb
izzyK <g00424523@atu.ie>
2025-03-06 19:34:12 +0000

    Add basic outline of methods for computer/router classes

--- NetworkSimulation/src/Network/Router.java
+++ NetworkSimulation/src/Network/Router.java
@@ -1,5 +1,16 @@
 package Network;
 
-public class Router {
+import java.util.HashMap;
+import java.util.Map;
 
+// Simulates a Router
+public class Router {
+	
+	// HashMap to store routing table (connected devices and their IP addresses)
+    private Map<String, Computer> routingTable = new HashMap<>();
+
+    public void connectDevice(Computer device) {
+        //method to connect device to router
+    	//store devices & their IPs in routing table
+    }
 }
